3214c742fe8406115ee17d6bac07dd9f7cf96ccf
ginkyenglee <jinkyung908@gmail.com>
2022-01-02 18:25:54 +0900

    contents, application split
    1. 컨텐츠를 넣는 부분을 확실하게 분리
    2. 컨텐츠 추가되면 자동으로, 북마크 생성되도록
    
    Signed-off-by: ginkyenglee <jinkyung908@gmail.com>

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,5 +1,7 @@
 const article = document.querySelector("#article");
-const hash = location.hash
+const itemList = document.querySelector("#itemList");
+const hash = location.hash;
+
 function contentsFetch(itemName){
     fetch(`content-${itemName}`).then(function(response){
         if(response.status === 404){
@@ -12,6 +14,21 @@ function contentsFetch(itemName){
         }
     })
 }
+
+fetch('itemList').then(function(response){
+    response.text().then(function(text){
+        let tags = ''
+        const itemListValues = text.split(',');
+        for (const idx in itemListValues){
+            const itemName = itemListValues[idx];
+            const newList = `<li><a href="#!${itemName}" onclick="contentsFetch('${itemName}')">${itemName}</a></li>`
+            tags = tags + newList;
+        }
+
+        itemList.innerHTML = tags;
+    })
+})
+
 
 if (hash){
     contentsFetch(hash.substring(2));
