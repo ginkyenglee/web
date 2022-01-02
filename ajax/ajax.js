4360cefddf35a107a755f619e83d51898f53daea
ginkyenglee <jinkyung908@gmail.com>
2022-01-02 17:44:57 +0900

    hash bookmark
    1. 해쉬를 활용해서 주소를 통해 복사붙여넣기 했을 때, 사용자가 보던
    화면이 뜨게함.
    
    Signed-off-by: ginkyenglee <jinkyung908@gmail.com>

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,7 +1,6 @@
 const article = document.querySelector("#article");
-
+const hash = location.hash
 function contentsFetch(itemName){
-
     fetch(`content-${itemName}`).then(function(response){
         if(response.status === 404){
             alert('File Not Found')
@@ -12,4 +11,11 @@ function contentsFetch(itemName){
             })
         }
     })
+}
+
+if (hash){
+    contentsFetch(hash.substring(2));
+}
+else{
+    contentsFetch('welcome');
 }
